992a969639af7e661b95fad4edcf6f8a4d314a18
4ll5olved <abrahamwibowo2@gmail.com>
2025-01-26 19:23:11 +0700

    ready

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,23 @@
 import express, { Application } from "express";
 import { PORT } from "./config";
 import cors from "cors";
+import { eventRoutes } from "./routes/event.routes";
 export class App {
     private app: Application
     constructor() {
         this.app = express()
         this.configure()
+        this.routes()
     }
 
     private configure() {
         this.app.use(express.json())
         this.app.use(cors());
+        
+    }
 
+    private routes() {
+        this.app.use("/api/event", eventRoutes());
     }
 
     start() {
