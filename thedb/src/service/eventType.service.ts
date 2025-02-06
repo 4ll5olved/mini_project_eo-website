4fbc61425fba173cdb19df47873aefe6a6c48831
4ll5olved <abrahamwibowo2@gmail.com>
2025-02-06 11:09:53 +0700

    schema generated and updated

--- thedb/src/service/eventType.service.ts
+++ thedb/src/service/eventType.service.ts
@@ -1,4 +1,4 @@
-import { Prisma, EventStatus} from "@prisma/client";
+import { Prisma } from "@prisma/client";
 import { Request } from "express";
 import { prisma } from "../config";
 import { pagination } from "../helper/pagination";
